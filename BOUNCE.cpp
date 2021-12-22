6a6136be9ab726278acc6f507077444d992cab6e
Prashant007-git <87236676+Prashant007-git@users.noreply.github.com>
2021-12-22 18:01:08 +0530

    Create BOUNCE.cpp

---
+++ BOUNCE.cpp
@@ -0,0 +1,42 @@
+#include<dos.h>
+#include<stdlib.h>
+#include<iostream.h>
+#include<graphics.h>
+#include<math.h>
+#include<conio.h>
+#include<unistd.h>
+
+int main()
+{
+	int gd=DETECT,gm;
+	initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
+	float x=1,y=0.0000,j=0.5,count=0.1;
+	float r=15;
+	setcolor(14);
+	line(0,215,650,215);
+	sleep(1);
+	for(int k=0;k<=7;k++)
+	{
+		for(float i=90;i<270;i+=10)
+		{
+			y=cos((i*22/7)/180)/j;
+			if(y>0)
+			y=y;
+			x+=5;
+			setcolor(14);
+			setfillstyle(1,14);
+			circle(x,y*100+200,r);
+			floodfill(x,y*100+200,14);
+			delay(100);
+			setcolor(0);
+			setfillstyle(1,0);
+			circle(x,y*100+200,r);
+			floodfill(x,y*100+200,0);
+		}
+		j+=count;
+		count+=0.1;
+	}
+	getch();
+	return 0;
+	closegraph();
+}
